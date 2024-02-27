bf4ffeda98d92908b26955c6802e446328727500
Matías Sánchez Rivas <matiassanchezrivas@gmail.com>
2024-02-28 09:27:09 +1100

    Add theme

---
+++ src/theme/common.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+import {colors} from './theme';
+
+export const commonStyles = StyleSheet.create({
+  iconStyle: {
+    color: colors.primary,
+  },
+  text: {
+    color: colors.textPri,
+  },
+});
---
+++ src/theme/theme.ts
@@ -0,0 +1,23 @@
+export const colors = {
+  backgroundPri: '#f6f6f6',
+  backgroundSec: '#ffffff',
+  primary: '#0170ff',
+  textPri: '#121212',
+  textSec: '#686868',
+};
+
+export const spacing = {
+  xs: 4,
+  s: 8,
+  m: 16,
+  l: 24,
+  xl: 36,
+  xxl: 48,
+};
+
+export const radius = {
+  xs: 3,
+  s: 5,
+  m: 10,
+  l: 15,
+};
